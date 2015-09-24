745be13bc37789a4f84d3a6437fafac8c0733e10
ChefDK User <chef@ip-172-31-7-28.ec2.internal>
2015-09-24 20:27:47 +0000

    Added additional tests

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -11,5 +11,13 @@ describe 'apache::default' do
     it { should be_enabled }
     it { should be_running }
   end
+
+  describe port(80) do
+    it { should be_listening }
+  end
+
+  describe command("curl http://localhost") do
+    its(:stdout) { should match /Hello, world!/ }
+  end
   
 end
