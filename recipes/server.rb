f7980b61122dcd30f0da33e503ce32795dac953e
ChefDK User <chef@ip-172-31-7-28.ec2.internal>
2015-09-24 18:10:11 +0000

    moved server.rb file and updated apt-get in recipe

---
+++ recipes/server.rb
@@ -0,0 +1,12 @@
+execute "apt-get update"
+
+package "apache2"
+
+file "/var/www/html/index.html" do
+	content "<h1>Hello, world!</h1>"
+	action :create
+end
+
+service "apache2" do
+	action [ :enable, :start ]
+end
