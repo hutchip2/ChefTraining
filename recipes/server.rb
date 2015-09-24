2823de7517f5af83724f8baba6f89696aa19209e
ChefDK User <chef@ip-172-31-7-28.ec2.internal>
2015-09-24 21:16:58 +0000

    Added information to server page and updated minor version

--- recipes/server.rb
+++ recipes/server.rb
@@ -3,7 +3,11 @@ execute "apt-get update"
 package "apache2"
 
 file "/var/www/html/index.html" do
-	content "<h1>Hello, world!</h1>"
+	content "<h1>Hello, world!</h1>
+	
+        IPADDRESS: #{node["ipaddress"]}
+        HOSTNAME : #{node["hostname"]}
+"
 	action :create
 end
 
